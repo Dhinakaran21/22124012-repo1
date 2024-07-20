47a29b6834cb52277fd626dfd5797c2d1c033632
Dhinakaran21 <dhinakaran21052005@gmail.com>
2024-07-20 08:37:17 +0530

    dhina.java

---
+++ dhina.java
@@ -0,0 +1,11 @@
+public class dhina {
+ public static void main(String[] args){
+string hungry = "yes";
+if (hungry == "yes";)
+system.out.println("eat something...");
+else{
+system.out.println("Do work.....");
+
+}
+}
+}
