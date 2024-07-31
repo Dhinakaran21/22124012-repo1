5b9f7f59d2f970f617aede0ac08e168d360aae7a
Dhinakaran21 <dhinakaran21052005@gmail.com>
2024-07-31 15:03:30 +0530

    Update dhina.java

--- dhina.java
+++ dhina.java
@@ -1,11 +1,9 @@
 public class dhina {
- public static void main(String[] args){
-string hungry = "yes";
-if (hungry == "yes";)
-system.out.println("eat something...");
-else{
-system.out.println("Do work.....");
-
-}
-}
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
